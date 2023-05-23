3a67c9b5735944ade1d1b7e7d583521eccc5b798
donggeon <ndmb2012@gmail.com>
2023-05-23 19:53:28 +0900

    first

---
+++ src/Std_Lecture/Lecture_std.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Content_TopBar from '../Std_Common/Content_TopBar_std';
+import Content_Heading_Name_std from '../Std_Common/Content_Heading_Name_std';
+import Content_Footer_std from '../Std_Common/Content_Footer_std';
+import Lecture_Finish_std from './Lecture_Finish_std';
+import Lecture_Not_Finish_std from './Lecture_Not_Finish_std';
+
+function Lecture_std() {
+  
+  return (
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        {/* <!-- Main Content --> */}
+        <div id="content">
+
+            {/* TopBar */}
+            <Content_TopBar></Content_TopBar>
+            {/* <!-- End of Topbar --> */}
+
+            {/* <!-- Begin Page Content --> */}
+            <div class="container-fluid">
+
+                {/* 헤딩 이름 부분 */}
+                <Content_Heading_Name_std></Content_Heading_Name_std>
+                
+                {/* <!-- Content Row --> */}
+                <div class="row">
+                    {/* 강의 목록 */}
+                    <div class="col-xl-12 col-md-12 mb-4">
+                        <div class="card border-left-warning shadow h-100 py-2">
+                            <div class="card-header py-3">
+                                <h3 class="m-0 font-weight-bold text-warning">강의 목록</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+
+                                    {/* <!-- Content Column --> */}
+                                    <div class="col-lg-6 my-2">
+            
+                                        <div class="card shadow my-4">
+                                            <div class="card-header py-3">
+                                                <h3 class="m-0 font-weight-bold text-primary">finish</h3>
+                                            </div>
+                                            
+                                            {/* 시청한 강의 */}
+                                            <Lecture_Finish_std></Lecture_Finish_std>
+
+                                        </div>
+                                    </div>
+            
+                                    <div class="col-lg-6 my-2">
+            
+                                        <div class="card shadow my-4">
+                                            <div class="card-header py-3">
+                                                <h3 class="m-0 font-weight-bold text-primary">not finish</h3>
+                                            </div>
+                                            
+                                            {/* 시청안한 강의 */}
+                                            <Lecture_Not_Finish_std></Lecture_Not_Finish_std>
+
+                                        </div>
+            
+                                    </div>
+                                </div>
+            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            {/* <!-- /.container-fluid --> */}
+
+        </div>
+        {/* <!-- End of Main Content --> */}
+
+        {/* <!-- Footer --> */}
+        <Content_Footer_std></Content_Footer_std>
+        {/* <!-- End of Footer --> */}
+
+    </div>
+  )
+}
+export default Lecture_std;
---
+++ src/Std_Video/Video_Watching_std.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+function Video_Watching_std() {
+    const [dataList, setDataList] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const fetchDataList = async () => {
+        try {
+            // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+            setError(null);
+            setDataList(null);
+            // loading 상태를 true 로 바꿉니다.
+            setLoading(true);
+            const response = await axios.get(
+                'https://494f28fe-80a6-4d36-9cec-5b3a50cd7b3d.mock.pstmn.io/lectures' // Backend api classroom_id로 개별 정보 받아오기
+            );
+            setDataList(response.data); // 데이터는 response.data 안에 들어있습니다.
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false);
+    };
+  
+    useEffect(() => { // 한번만 렌더하기 위해서
+        fetchDataList();
+    }, []);
+
+  return (
+    <div class="col-xl-12 col-md-12 mb-4">
+        <div class="card border-left-warning shadow h-800 py-2">
+            <div class="card-header py-3">
+                <h3 class="m-0 font-weight-bold text-warning">강의</h3>
+            </div>
+            <div class="card-body text-center">
+                {dataList?.map((data, index) => (
+                    <div style={{width: '100%', maxWidth: '800px', height: '500px', textAlign: 'center'}}>
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        // src="https://www.youtube.com/embed/fAe_Boxk61Q"
+                        src= {data.link}
+                        // 강의 하나만 불러오게 해야함
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Video_Watching_std;
